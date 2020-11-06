a5326daea91a84877a7b57c21c8124d18a854525
Jake Ruth <jakeruthmusic@gmail.com>
2020-11-06 13:16:25 -0500

    testing heroku postbuild

---
+++ frontend/build/precache-manifest.ee06ccfb8d9927a2752d88aca6b138c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2d246e29b41bde85e0781f2c0e9cbdf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "23f9ab8179e720aff735",
+    "url": "/static/css/main.ef639995.chunk.css"
+  },
+  {
+    "revision": "11db2baaed34c5891c8c",
+    "url": "/static/js/2.28b1b8ad.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.28b1b8ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23f9ab8179e720aff735",
+    "url": "/static/js/main.f16003a8.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
