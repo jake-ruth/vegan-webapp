279d668f3ac2c8abecdeeb2d22306960ba7e1c34
Jake Ruth <jakeruthmusic@gmail.com>
2020-11-11 18:40:19 -0500

    test

---
+++ frontend/build/precache-manifest.28bb18112501ad180bc4be51b7ca7627.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f9acee616ecc71c56e72ad146f6551c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "501c921fc00fd8e906dc",
+    "url": "/static/css/main.38749b45.chunk.css"
+  },
+  {
+    "revision": "32b7ef024b49131802d4",
+    "url": "/static/js/2.0152115a.chunk.js"
+  },
+  {
+    "revision": "1937a0fc6634269e5f47f8181e27fdc7",
+    "url": "/static/js/2.0152115a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "501c921fc00fd8e906dc",
+    "url": "/static/js/main.525b7c32.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
