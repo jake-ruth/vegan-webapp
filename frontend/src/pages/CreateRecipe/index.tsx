da7667eefd50020c14f409a3d679c2993f0282ca
Jake Ruth <jakeruthmusic@gmail.com>
2020-11-11 19:34:26 -0500

    working on create recipe page

--- frontend/src/pages/CreateRecipe/index.tsx
+++ frontend/src/pages/CreateRecipe/index.tsx
@@ -54,85 +54,93 @@ export const CreateRecipe = () => {
   return (
     <div>
       <Navbar />
-      <form onSubmit={handleSubmit(onSubmit)} className='login-page__card'>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <h1 style={{ fontStyle: 'italic', textAlign: 'center' }}>Create Recipe</h1>
         <div className='create-recipe'>
-          <Controller
-            as={TextField}
-            control={control}
-            rules={{ required: 'Title Required' }}
-            type='text'
-            id='title'
-            name='title'
-            label='Title'
-          />
-          <div className='error'>{errors.title && errors.title.message}</div>
+          <div className='create-recipe__content'>
+            <div className='create-recipe__section'>
+              <Controller
+                as={TextField}
+                control={control}
+                rules={{ required: 'Title Required' }}
+                type='text'
+                id='title'
+                name='title'
+                label='Title'
+              />
+              <div className='error'>{errors.title && errors.title.message}</div>
 
-          <Controller
-            as={TextField}
-            control={control}
-            multiline
-            variant='outlined'
-            rows={5}
-            name='description'
-            label='Description'
-            rules={{ required: 'Description Required' }}
-          />
-          <div className='error'>{errors.description && errors.description.message}</div>
+              <Controller
+                as={TextField}
+                control={control}
+                multiline
+                variant='outlined'
+                rows={5}
+                style={{ marginTop: 20 }}
+                name='description'
+                label='Description'
+                rules={{ required: 'Description Required' }}
+              />
+              <div className='error'>{errors.description && errors.description.message}</div>
 
-          <Controller
-            as={TextField}
-            control={control}
-            multiline
-            variant='outlined'
-            rows={5}
-            name='instructions'
-            label='Instructions'
-            rules={{ required: 'Instructions Required' }}
-          />
-          <div className='error'>{errors.description && errors.description.message}</div>
+              <Controller
+                as={TextField}
+                control={control}
+                multiline
+                variant='outlined'
+                rows={5}
+                style={{ marginTop: 20 }}
+                name='instructions'
+                label='Instructions'
+                rules={{ required: 'Instructions Required' }}
+              />
+              <div className='error'>{errors.description && errors.description.message}</div>
+            </div>
 
-          <h3 style={{ fontStyle: 'italic' }}>Ingredients</h3>
+            <div className='create-recipe__section'>
+              <h3 style={{ fontStyle: 'italic' }}>Ingredients</h3>
 
-          <ul>
-            {fields.map((ingredient, index) => (
-              <li key={index}>
-                <Controller as={TextField} name={`ingredients[${index}].value`} control={control} />
+              <ul>
+                {fields.map((ingredient, index) => (
+                  <li key={index}>
+                    <Controller as={TextField} name={`ingredients[${index}].value`} control={control} />
 
-                <Button
-                  variant='contained'
-                  size='small'
-                  style={{ borderRadius: 0 }}
-                  color='secondary'
-                  onClick={() => remove(index)}>
-                  Delete
-                </Button>
-              </li>
-            ))}
-          </ul>
+                    <Button
+                      variant='contained'
+                      size='small'
+                      style={{ borderRadius: 0 }}
+                      color='secondary'
+                      onClick={() => remove(index)}>
+                      Delete
+                    </Button>
+                  </li>
+                ))}
+              </ul>
 
-          <Button type='button' onClick={() => append({ value: '' })}>
-            Add Ingredient
-          </Button>
+              <Button type='button' size='small' onClick={() => append({ value: '' })}>
+                Add Ingredient
+              </Button>
 
-          <Divider />
-          <h3 style={{ fontStyle: 'italic' }}>Prep Time</h3>
-          <div style={{ display: 'flex' }}>
-            <Controller as={TextField} control={control} type='number' name='prepHours' label='Hours' defaultValue={0} />
-            <Controller as={TextField} control={control} type='number' name='prepMinutes' label='Minutes' defaultValue={0} />
-          </div>
+              <Divider />
+              <h3 style={{ fontStyle: 'italic' }}>Prep Time</h3>
+              <div style={{ display: 'flex' }}>
+                <Controller as={TextField} control={control} type='number' name='prepHours' label='Hours' defaultValue={0} />
+                <Controller as={TextField} control={control} type='number' name='prepMinutes' label='Minutes' defaultValue={0} />
+              </div>
 
-          <h3 style={{ fontStyle: 'italic' }}>Cook Time</h3>
-          <div style={{ display: 'flex' }}>
-            <Controller as={TextField} control={control} type='number' name='cookHours' label='Hours' defaultValue={0} />
-            <Controller as={TextField} control={control} type='number' name='cookMinutes' label='Minutes' defaultValue={0} />
+              <h3 style={{ fontStyle: 'italic' }}>Cook Time</h3>
+              <div style={{ display: 'flex' }}>
+                <Controller as={TextField} control={control} type='number' name='cookHours' label='Hours' defaultValue={0} />
+                <Controller as={TextField} control={control} type='number' name='cookMinutes' label='Minutes' defaultValue={0} />
+              </div>
+            </div>
           </div>
 
           <br />
 
           <Controller as={TextField} control={control} name='yieldAmount' label='Yield' />
 
-          <Button type='submit' variant='contained' color='primary' style={{ borderRadius: 0 }}>
+          <Button type='submit' variant='contained' color='primary' style={{ borderRadius: 0, maxWidth: 200, marginTop: '1em' }}>
             Save Recipe!
           </Button>
         </div>
